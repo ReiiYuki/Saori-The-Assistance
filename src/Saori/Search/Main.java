4b2dda23385369f5246429c624c47d103a0850ff
PaiizZ <paiizzmk@gmail.com>
2015-05-07 14:24:40 +0700

    Finish SearchFile

--- src/Saori/Search/Main.java
+++ src/Saori/Search/Main.java
@@ -1,22 +1,25 @@
 package Saori.Search;
+
+import java.awt.EventQueue;
+
+import Saori.Calculator.CalculatorUI;
+
 public class Main {
 	/**
 	 * to run main for show DigitalClockUI
 	 * @param args is argument for run String
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("12");
-
-		FindFileUI findfile = new FindFileUI();
-		findfile.pack();
-		findfile.setVisible( true );
-
-=======
-		FindFile findfile = new FindFile();
-		FindFileUI findfileUI = new FindFileUI(findfile);
-		findfileUI.pack();
-		findfileUI.setVisible( true );
->>>>>>> 226ae6b2b8bd59151578113b4f518804a352d8ae
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FindFile findfile = new FindFile();
+					FindFileUI findfileUI = new FindFileUI(findfile);
+					findfileUI.setVisible( true );
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
