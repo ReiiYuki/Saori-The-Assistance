237fa5db9296c0a4c09aadbf57cf3dd765687b87
ReiiYuki <iguru.shou.5410070@gmail.com>
2015-05-08 18:42:35 +0700

    Fixed bug

--- src/Saori/Calculator/Main.java
+++ src/Saori/Calculator/Main.java
@@ -2,9 +2,6 @@ package Saori.Calculator;
 
 import java.awt.EventQueue;
 
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-
 public class Main {
 	public static void main(String args[]) {
 	
